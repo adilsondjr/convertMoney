5cd3f49172f14246308bea0907e064fb88bf553c
Adilson Domiciano <adilsondjr@gmail.com>
2020-05-27 17:15:24 -0300

    montando a primeira pagina do projeto com ejs

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const path = require('path')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(3000, err => {
     if(err) {
