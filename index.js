ea1fc9e006c6665ae21c2a63dce8ed7b41db35b8
Adilson Domiciano <adilsondjr@gmail.com>
2020-05-27 18:22:43 -0300

    Criando uma ação no formulario para enviar os dados da cotação Criando funções para fazer a conversão Criando a view de cotação para exibir o resultado da cotação Criando um novo endpoint cotacao

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const convert = require('./lib/convert')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    const conversao = convert.convert(cotacao, quantidade)
+    res.render('cotacao', {
+        cotacao,
+        quantidade,
+        conversao
+    })
 })
 
 app.listen(3000, err => {
