42186a2ebd73aa319c7303b29a93f1b64b8a3ce0
Adilson Domiciano <adilsondjr@gmail.com>
2020-05-27 17:01:36 -0300

    tratamento do path para que funcione no zeit now

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.set('view engine', 'ejs')
-
+app.set('views', path.join(__dirname, 'views'))
 
 app.listen(3000, err => {
     if(err) {
